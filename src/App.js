fb6d12a8cc32d5d96a14c05a5dec1697b2db9dab
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-15 04:48:48 +0600

    adding header component and navlinks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component";
 import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
 import "./App.css";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page </h1>
-  </div>
-);
-
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
