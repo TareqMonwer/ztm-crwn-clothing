dd2139fe3fc7ec36dc4b3ad6107e7e8443b81826
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-15 05:50:26 +0600

    custom form input with styles

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
+import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -27,23 +29,23 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="email"
             required
           />
-          <label>Email</label>
 
-          <input
+          <FormInput
             name="password"
             type="password"
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             value={this.state.password}
+            label="password"
             required
           />
-          <label>Password</label>
 
           <input type="submit" value="Submit" />
         </form>
