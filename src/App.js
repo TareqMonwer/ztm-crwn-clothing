c3159f320762b49ceed3a9c445db577096ee9987
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-21 14:51:17 +0600

    google signin-signout with firebase|

--- src/App.js
+++ src/App.js
@@ -4,19 +4,44 @@ import SigninAndSignup from "./components/pages/signin-and-signup/signin-signup.
 import Header from "./components/header/header.component";
 import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
+import { auth } from "./firebase/firebase.utils";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SigninAndSignup} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      // console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SigninAndSignup} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
